69db2f6587e876d97b41d0bbebf865919f1c724d
mickeychu <binhminh88@gmail.com>
2015-09-18 19:37:14 +0300

    badminton page update

--- badminton.php
+++ badminton.php
@@ -1,11 +1,59 @@
+<?php 
+//Collect data to show full list¨
+    // Connect to the MySQL database  
+    require "scripts/connect.php";
+$username = "mickey";
+$email_list = "";
+$sql = mysqli_query($link, "SELECT * FROM `badminton_list`");
+$number = 0;
+$email_count = mysqli_num_rows($sql); // count the amount of email in database
+if ($email_count > 0) {
+	while($row = mysqli_fetch_array($sql, MYSQLI_ASSOC)) {
+		$email_ID = $row["id"];
+		$email = $row["email"];
+		$number++;
+		$email_list .='<tr>
+						  <td width="59">'.$number.'</td>
+						  <td width="419" align="center">'.$email.'</td>
+						  <td width="125"><input type="checkbox" name="check[]" value="'.$email.'" style="transform: scale(1.5)"></td>
+					   </tr>';
+		}
+}
+else {
+	$email_list = "You have no email yet!";
+}
+?>
+<?php
+if (isset ($_POST['badminton']))
+{
+	$emails = $_POST['check'];
+	$username = "mickey";
+	foreach($emails as $email)
+	{
+		$sql = "INSERT INTO `jolly`.`badminton_list` (`id`, `email`, `username`) VALUES (NULL, '$email', '$username');";
+ 		$query = mysqli_query($link, $sql) or die(mysqli_error($link));
+	}
+	echo "imported OK!!!";
+	exit();
+}
 
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Import Gmail Contact</title>
+<title>ADMIN AREA</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
+<script type="text/javascript">
+	function delete_id(id)
+	{
+		 if(confirm('Sure To Remove This Article ?'))
+		 {
+		  	window.location.href='index.php?deleteID='+id;
+		 }
+	}
+</script>
 <link href="css/tooplate_style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
@@ -33,92 +81,24 @@
     <div class="content_bottom"></div>
     <div class="content_top"><span id="portfolio"></span></div>
     <div class="content">
-    <?php
-$client_id = '822743733345-84qc52psgjnadlk19kilngibbu84nr48.apps.googleusercontent.com';
-$client_secret = 'c1MABQBP8vUa2LiHF5DV3b_X';
-$redirect_uri = 'http://localhost/import-gmail-contact/oauth.php';
-$max_results = 10;
-
-$auth_code = $_GET["code"];
-
-function curl_file_get_contents($url)
-{
- $curl = curl_init();
- $userAgent = 'Mozilla/40.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)';
- 
- curl_setopt($curl,CURLOPT_URL,$url);	//The URL to fetch. This can also be set when initializing a session with curl_init().
- curl_setopt($curl,CURLOPT_RETURNTRANSFER,TRUE);	//TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
- curl_setopt($curl,CURLOPT_CONNECTTIMEOUT,5);	//The number of seconds to wait while trying to connect.	
- 
- curl_setopt($curl, CURLOPT_USERAGENT, $userAgent);	//The contents of the "User-Agent: " header to be used in a HTTP request.
- curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);	//To follow any "Location: " header that the server sends as part of the HTTP header.
- curl_setopt($curl, CURLOPT_AUTOREFERER, TRUE);	//To automatically set the Referer: field in requests where it follows a Location: redirect.
- curl_setopt($curl, CURLOPT_TIMEOUT, 10);	//The maximum number of seconds to allow cURL functions to execute.
- curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);	//To stop cURL from verifying the peer's certificate.
- curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
- 
- $contents = curl_exec($curl);
- curl_close($curl);
- return $contents;
-}
-
-$fields=array(
-    'code'=>  urlencode($auth_code),
-    'client_id'=>  urlencode($client_id),
-    'client_secret'=>  urlencode($client_secret),
-    'redirect_uri'=>  urlencode($redirect_uri),
-    'grant_type'=>  urlencode('authorization_code')
-);
-$post = '';
-foreach($fields as $key=>$value) { $post .= $key.'='.$value.'&'; }
-$post = rtrim($post,'&');
-
-$curl = curl_init();
-curl_setopt($curl,CURLOPT_URL,'https://accounts.google.com/o/oauth2/token');
-curl_setopt($curl,CURLOPT_POST,5);
-curl_setopt($curl,CURLOPT_POSTFIELDS,$post);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER,TRUE);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER,0);
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
-$result = curl_exec($curl);
-curl_close($curl);
-
-$response =  json_decode($result);
-$accesstoken = $response->access_token;
-
-$url = 'https://www.google.com/m8/feeds/contacts/default/full?max-results='.$max_results.'&oauth_token='.$accesstoken;
-$xmlresponse =  curl_file_get_contents($url);
-if((strlen(stristr($xmlresponse,'Authorization required'))>0) && (strlen(stristr($xmlresponse,'Error '))>0)) //At times you get Authorization error from Google.
-{
-	echo "<h2>OOPS !! Something went wrong. Please try reloading the page.</h2>";
-	exit();
-}
-echo "<h3>Email Addresses:</h3>";
-$xml =  new SimpleXMLElement($xmlresponse);
-$xml->registerXPathNamespace('gd', 'http://schemas.google.com/g/2005');
-$result = $xml->xpath('//gd:email');
-$email_list = '';
-$count = 1;
-foreach ($result as $title) {
-  $email_list.= '<tr>
-  					<td width="413">'.$count.'--'.$title->attributes()->address.'</td>
-      				<td width="241"><input type="checkbox" name="check[]" value="badminton" style="transform: scale(1.5)"></td>
-	  			</tr>';
-  
-  $count++;
-}
-?>
-
-<table width="670" border="1">
+      <h1 style="font-size:18px">Welcome <span style="color:#4C5EED; text-transform:uppercase"><?php echo $username?></span></h1>
+      <form action="list.php" method="post">
+		<table width="625" height="147" border="1">
   <tbody>
-<?php echo $email_list;?>
     <tr>
-      <td><input type="button" name="button" id="button" value="Import selected to badminton group" /></td>
+      <td colspan="3">&nbsp;</td>
+      </tr>
+	<?php echo $email_list;?>
+    <tr>
+      <td colspan="3">
+      	<input type="hidden" name ="badminton" value="badminton"/>
+      	<input type="submit" name="add" id="add" value="Send Invitation" style="width:auto" />
+      </td>
       </tr>
   </tbody>
 </table>
 
-
+      </form>
       <div class="cleaner"></div>
     </div>
     <div class="content_bottom"></div>
---
+++ import.php
@@ -0,0 +1,48 @@
+<?php
+/***** EDIT BELOW LINES *****/
+
+$xls_filename = 'export_'.date('Y-m-d').'.xls'; // Define Excel (.xls) file name
+ 
+    // Connect to the MySQL database  
+    require "scripts/connect.php";
+ 
+// Header info settings
+header("Content-Type: application/xls");
+header("Content-Disposition: attachment; filename=$xls_filename");
+header("Pragma: no-cache");
+header("Expires: 0");
+ 
+/***** Start of Formatting for Excel *****/
+// Define separator (defines columns in excel &amp; tabs in word)
+$sep = "\t"; // tabbed character
+ 
+// Start of printing column names as names of MySQL fields
+for ($i = 0; $i<mysql_num_fields($result); $i++) {
+  echo mysql_field_name($result, $i) . "\t";
+}
+print("\n");
+// End of printing column names
+ 
+// Start while loop to get data
+while($row = mysql_fetch_row($result))
+{
+  $schema_insert = "";
+  for($j=0; $j<mysql_num_fields($result); $j++)
+  {
+    if(!isset($row[$j])) {
+      $schema_insert .= "NULL".$sep;
+    }
+    elseif ($row[$j] != "") {
+      $schema_insert .= "$row[$j]".$sep;
+    }
+    else {
+      $schema_insert .= "".$sep;
+    }
+  }
+  $schema_insert = str_replace($sep."$", "", $schema_insert);
+  $schema_insert = preg_replace("/\r\n|\n\r|\n|\r/", " ", $schema_insert);
+  $schema_insert .= "\t";
+  print(trim($schema_insert));
+  print "\n";
+}
+?>
